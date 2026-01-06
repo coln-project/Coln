89dc212a89fe5775f9687663255e99d6fecf3af3
Owen Lynch <root@owenlynch.org>
2026-01-06 12:39:48 -0500

    more thoughts about names and core

--- geolog-lang/src/Geolog/Core.hs
+++ geolog-lang/src/Geolog/Core.hs
@@ -4,6 +4,20 @@ import Geolog.Common
 
 data Abs a = Abs Name a
 
+-- How do we deal with the fact that, in general, a top-level binding is for a
+-- judgment in the theory?
+
+-- I guess evaluation by default returns a VTmJ/VTyJ, and then we coerce it
+-- to a VTm when necessary?
+
+-- Alternatively, we could have JLam as a constructor of VTm (VJLam).
+
+-- What is the right way to handle argument telescopes in nbe?
+
+-- Thought: named theory telescopes are the natural unit of subtyping. If we
+-- want theories with arguments, they should desugar to specializations of
+-- telescopes.
+
 data Tm
   = Local BId
   | Global QName
