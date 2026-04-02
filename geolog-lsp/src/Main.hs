4697e47d352612311c9441cfff791ffc46d48cd3
George Thomas <georgefsthomas@gmail.com>
2026-04-02 11:25:51 +0100

    Remove debugging code

--- geolog-lsp/src/Main.hs
+++ geolog-lsp/src/Main.hs
@@ -1,13 +1,10 @@
+import Data.IORef (newIORef)
 import Geolog.LSP (serverDefinition)
 import Geolog.LSP.Types (LSPState (..))
 import Language.LSP.Server (runServer)
-import Data.IORef (newIORef)
-import GHC.Conc (setUncaughtExceptionHandler)
-import Control.Exception
 
 main :: IO Int
 main = do
-  -- setUncaughtExceptionHandler (\e -> writeFile "/home/patrick/Documents/geolog/lsp.log" $ show e )
   ref <- newIORef mempty
   runServer $
     serverDefinition
