9137140e37d83f49e2868a9a5caa2a91f3e17d67
James Deikun <james.deikun@gmail.com>
2026-05-21 22:59:14 -0400

    finish Readback, create Print

---
+++ geolog-lang/src/Geolog/Core/Print.hs
@@ -0,0 +1,87 @@
+module Geolog.Core.Print where
+
+import Data.String (fromString)
+import Data.Text qualified as T
+import Diagnostician
+import FNotation qualified as N
+import FNotation.Names
+import Geolog.Common
+import Geolog.Core.Params
+import Geolog.Core.Syntax
+import Geolog.Notation
+
+-- Pretty printing
+--------------------------------------------------------------------------------
+
+type Names = Bwd Name
+
+class ToNotation a where
+  toNotation :: Names -> a -> N.Ntn0
+
+instance ToNotation BId where
+  toNotation xs (BId i) = go xs i []
+   where
+    go (_ :> x) 0 prev = N.Ident (x{last = x.last <> disamb}) ()
+     where
+      nx = length $ filter (== x) prev
+      disamb = if nx > 0 then "^" <> T.pack (show nx) else ""
+    go (xs' :> x) n prev = go xs' (n - 1) (x : prev)
+    go BwdNil _ _ = error $ "name " ++ show i ++ " not bound. ?names = " ++ (show $ toList xs)
+
+instance ToNotation (El e) where
+  toNotation xs = \case
+    LocalVar i -> toNotation xs i
+    GlobalVar x _ -> N.Ident x ()
+    Code ty -> toNotation xs ty
+    App f t -> N.Juxt (toNotation xs f) (toNotation xs t)
+    Lam _ (Abs x t) ->
+      N.Infix (N.Ident x ()) (N.Keyword "=>" ()) (toNotation (xs :> x) t)
+    Lam _ (AbsConst t) ->
+      N.Infix (N.Ident "_" ()) (N.Keyword "=>" ()) (toNotation xs t)
+    Proj t f -> N.Juxt (toNotation xs t) (N.Field f ())
+    Cons d ->
+      N.Tuple [field y t | (y, t) <- toList d] ()
+     where
+      field y t = N.Infix (N.Ident y ()) (N.Keyword ":" ()) (toNotation xs t)
+    Lit (LitInt i) -> N.Int i ()
+    Lit (LitString s) -> N.String s ()
+    Is t -> toNotation xs t -- invisible
+
+nbinding :: Name -> N.Ntn0 -> N.Ntn0
+nbinding x n = N.Infix (N.Ident x ()) (N.Keyword ":" ()) n
+
+instance ToNotation (Ty e) where
+  toNotation xs = \case
+    U u -> N.Keyword (fromString $ show $ decodesInto u) ()
+    Decode t -> toNotation xs t
+    Function f -> case f.cod of 
+      Abs x b -> N.Infix
+        (nbinding x (toNotation xs f.dom))
+        (N.Keyword "->" ())
+        (toNotation (xs :> x) b)
+      AbsConst b -> N.Infix
+        (toNotation xs f.dom)
+        (N.Keyword "->" ())
+        (toNotation xs b)
+    Record r -> N.Block "sig" Nothing (go xs $ toList r.fieldTypes) ()
+     where
+      go _ [] = []
+      go xs' ((y, a) : pairs') =
+        nbinding y (toNotation xs' a) : go (xs' :> y) pairs'
+    Eq eq -> N.Infix
+      (toNotation xs eq.lhs)
+      (N.Keyword "=" ())
+      (toNotation xs eq.rhs)
+    BuiltinTy a -> N.Keyword (fromString $ show a) ()
+
+class DPrettyWithNames a where
+  dprettyWithNames :: Names -> a -> DDoc
+
+instance DPrettyWithNames BId where
+  dprettyWithNames xs t = N.dprettyWithConfigs parseConfig lexConfig $ toNotation xs t
+
+instance DPrettyWithNames (El e) where
+  dprettyWithNames xs t = N.dprettyWithConfigs parseConfig lexConfig $ toNotation xs t
+
+instance DPrettyWithNames (Ty e) where
+  dprettyWithNames xs t = N.dprettyWithConfigs parseConfig lexConfig $ toNotation xs t
--- geolog-lang/src/Geolog/Core/Readback.hs
+++ geolog-lang/src/Geolog/Core/Readback.hs
@@ -29,29 +29,57 @@ instance Readback (V.Description V.El) (S.El D) where
     V.Describe v -> readb n v
     V.Become v -> S.Is (readb n v)
 
-readbClo :: (Readback (V.Evaluation a c) (b c)) => CtxLen -> V.Clo a c -> S.Abs b c
-readbClo n = \case
+readbClo :: (Readback (V.Evaluation a c) (b c)) => CtxLen -> V.Ty N -> V.Clo a c -> S.Abs b c
+readbClo n dom = \case
+  V.Clo x l f -> S.Abs x $ readb (n + 1) (f (V.LSnoc l $ V.local (FId n) dom))
+  V.CloConst t -> S.AbsConst $ readb n t
 
 instance (V.HasEvaluation c) => Readback (V.El c) (S.El c) where
   readb n = \case
     V.Neu ne -> readb n ne.spine $ readb n ne.head
     V.Code a -> S.Code (readb n a)
-    V.Lam a body -> S.Lam (readb n a) $ case V.scase @c of
-      SNominative -> readbClo n body
-      SDescriptive -> readbClo n body
+    V.Lam dom body -> S.Lam (readb n dom) $ case V.scase @c of
+      SNominative -> readbClo n dom body
+      SDescriptive -> readbClo n dom body
     V.Cons d -> S.Cons $ case V.scase @c of
       SNominative -> readb n <$> d
       SDescriptive -> readb n <$> d
     V.Lit l -> S.Lit l
 
+instance Readback V.FunctionType (S.FunctionType S.Ty) where
+  readb n f = S.FunctionType
+    { S.variant = f.variant
+    , S.dom = readb n f.dom
+    , S.cod = readbClo n f.dom f.cod
+    }
+
+instance Readback V.RecordType (S.RecordType S.Ty) where
+  readb n r = S.RecordType
+    { S.level = r.level
+    , S.fieldTypes = Dict
+      { head = r.fieldTypes.head
+      , values = Vector.fromList $ go n r.capture r.fieldTypes.values
+      }
+    }
+    where
+      go i ls fs = if Vector.null fs
+        then []
+        else do
+          let ty = Vector.head fs ls
+          readb i ty : go (i + 1) (V.LSnoc ls $ V.local (FId i) ty) (Vector.tail fs)
+
+instance Readback V.EqualityType (S.EqualityType S.El S.Ty) where
+  readb n eq = S.EqualityType
+    { S.at = readb n eq.at
+    , S.lhs = readb n eq.lhs
+    , S.rhs = readb n eq.rhs
+    }
+
 instance (V.HasEvaluation c) => Readback (V.Ty c) (S.Ty c) where
   readb n = \case
     V.U u -> S.U u
     V.Decode ne -> S.Decode $ readb n ne.spine $ readb n ne.head
-    V.Function f -> S.Function $ S.FunctionType
-      { S.variant = f.variant
-      , S.dom = readb n f.dom
-      , S.cod = case V.scase @c of
-        SNominative -> readbClo n f.cod
-        SDescriptive -> readbClo n f.cod
-      }
+    V.Function f -> S.Function $ readb n f
+    V.Record r -> S.Record $ readb n r
+    V.Eq eq -> S.Eq $ readb n eq
+    V.BuiltinTy b -> S.BuiltinTy b
