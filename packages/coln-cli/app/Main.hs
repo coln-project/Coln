312cda428a585656269ab5c08895324fc9c79fd4
Patrick Aldis <patricktaldis@gmail.com>
2026-06-19 11:36:10 +0100

    Add REPL to coln-cli

--- packages/coln-cli/app/Main.hs
+++ packages/coln-cli/app/Main.hs
@@ -6,11 +6,13 @@ import Coln.CLI.Options
 import Coln.CLI.Check
 import Coln.CLI.GenerateTS
 import Coln.CLI.GenerateIR
+import Coln.CLI.REPL
 
 run :: Options -> IO ()
 run (GenerateTS opts) = generateTS opts
 run (GenerateIR opts) = generateIR opts
 run (Check opts) = check opts
+run Repl = runRepl
 
 main :: IO ()
 main = execParser optionsInfo >>= run
