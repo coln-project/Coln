2fc1bfb4da6253b849600b5ea5f0a4f0f472a02e
Alex Good <alex@patternist.xyz>
2026-06-19 14:07:25 +0100

    coln-js-runtime: Re-export StoreHandle from wrapper

--- packages/coln-js-runtime/src/ts/index.ts
+++ packages/coln-js-runtime/src/ts/index.ts
@@ -1,5 +1,5 @@
-export type { RowRef, RowView, Value, CommitResult } from "#wasm-bodge/bindings";
-export { valueEqual } from "#wasm-bodge/bindings"
+export type { RowRef, RowView, Value, CommitResult, CommitChunk } from "#wasm-bodge/bindings";
+export { StoreHandle, TransactionHandle, valueEqual } from "#wasm-bodge/bindings"
 
 export * as ColnSet from "./ColnSet";
 
