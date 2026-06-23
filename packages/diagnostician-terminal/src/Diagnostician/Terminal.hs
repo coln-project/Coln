614e1f81898e432eabe7caed2b242101877e14c0
George Thomas <georgefsthomas@gmail.com>
2026-06-23 15:34:13 +0100

    Print to terminal directly instead of constructing intermediate text

--- packages/diagnostician-terminal/src/Diagnostician/Terminal.hs
+++ packages/diagnostician-terminal/src/Diagnostician/Terminal.hs
@@ -2,9 +2,8 @@ module Diagnostician.Terminal (
   terminalReporter,
 ) where
 
-import Data.Text.Lazy.IO qualified as TL
 import Diagnostician
-import Prettyprinter (defaultLayoutOptions, layoutPretty)
+import Prettyprinter (hardline)
 import Prettyprinter.Render.Terminal qualified as Ansi
 import Prettyprinter.Render.Text qualified as Text
 import System.Console.ANSI (hSupportsANSI)
@@ -18,9 +17,9 @@ terminalReporter handle =
         isTerm <- hSupportsANSI handle
         let render =
               if isTerm
-                then Ansi.renderLazy . fmap (toAnsiStyle (codeMeta d.code).severity)
-                else Text.renderLazy
-        TL.hPutStr handle $ render (layoutPretty defaultLayoutOptions $ dpretty d) <> "\n"
+                then Ansi.putDoc . fmap (toAnsiStyle (codeMeta d.code).severity)
+                else Text.putDoc
+        render $ dpretty d <> hardline
     }
 
 toAnsiStyle :: Severity -> DiagnosticAnn -> Ansi.AnsiStyle
