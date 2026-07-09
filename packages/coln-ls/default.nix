6bbdd4365f4ca42e8fecbc7960e69ac0a9cce5f5
Patrick Aldis <patricktaldis@gmail.com>
2026-07-09 11:00:12 +0100

    remove coln-ls testing suite

--- packages/coln-ls/default.nix
+++ packages/coln-ls/default.nix
@@ -1,7 +1,6 @@
 { mkDerivation, base, coln-compiler, containers, diagnostician
-, exceptions, fnotation, lens, lib, lsp, lsp-test, megaparsec, mtl
-, prettyprinter, process, stm, tasty, tasty-hunit, text
-, transformers, vector
+, exceptions, fnotation, lens, lib, lsp, megaparsec, mtl
+, prettyprinter, stm, text, transformers, vector
 }:
 mkDerivation {
   pname = "coln-ls";
@@ -14,9 +13,6 @@ mkDerivation {
     lens lsp megaparsec mtl prettyprinter stm text transformers vector
   ];
   executableHaskellDepends = [ base ];
-  testHaskellDepends = [
-    base lsp-test process tasty tasty-hunit text
-  ];
   license = "(Apache-2.0 OR MIT)";
   mainProgram = "coln-language-server";
 }
