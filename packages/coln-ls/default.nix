97718caa0ebe6a98350b2fb0d46650c8298bdfba
Patrick Aldis <patricktaldis@gmail.com>
2026-07-13 15:25:22 +0100

    remove coln-lsp executable component

--- packages/coln-ls/default.nix
+++ packages/coln-ls/default.nix
@@ -6,13 +6,9 @@ mkDerivation {
   pname = "coln-ls";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     base coln-compiler containers diagnostician exceptions fnotation
     lens lsp megaparsec mtl prettyprinter stm text transformers vector
   ];
-  executableHaskellDepends = [ base ];
   license = "(Apache-2.0 OR MIT)";
-  mainProgram = "coln-language-server";
 }
