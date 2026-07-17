913e25eb1d064f3c972d28ba9bb2ef70f983dff6
mvr <mitchell.v.riley@gmail.com>
2026-07-17 16:25:55 +0100

    Add missing `tasty-hunit` to nix file

--- packages/coln-compiler/default.nix
+++ packages/coln-compiler/default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, base, bytestring, containers, diagnostician
 , directory, filepath, fnotation, hashable, lib, mtl
 , ordered-containers, prettyprinter, tasty, tasty-expected-failure
-, tasty-golden, temporary, text, vector, vector-hashtables
+, tasty-golden, tasty-hunit, temporary, text, vector, vector-hashtables
 }:
 mkDerivation {
   pname = "coln-compiler";
@@ -15,7 +15,7 @@ mkDerivation {
   testHaskellDepends = [
     base bytestring containers diagnostician filepath fnotation
     ordered-containers prettyprinter tasty tasty-expected-failure
-    tasty-golden temporary text vector
+    tasty-golden tasty-hunit temporary text vector
   ];
   license = "(Apache-2.0 OR MIT)";
 }
