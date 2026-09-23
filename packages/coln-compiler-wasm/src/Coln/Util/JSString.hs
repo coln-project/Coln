3ce0d6a03478d28a054b42d78f224812889ad2ae
Owen Lynch <root@owenlynch.org>
2026-09-23 15:05:59 +0100

    fixed formatting for coln-compiler-wasm

--- packages/coln-compiler-wasm/src/Coln/Util/JSString.hs
+++ packages/coln-compiler-wasm/src/Coln/Util/JSString.hs
@@ -1,49 +1,45 @@
 -- SPDX-FileCopyrightText: 2026 Coln contributors
 --
 -- SPDX-License-Identifier: Apache-2.0 OR MIT
-
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnliftedFFITypes #-}
 
-module Coln.Util.JSString
-  ( textToJSString
-  , byteStringToJSString
-  , textFromJSString
-  ) where
+module Coln.Util.JSString (
+  textToJSString,
+  byteStringToJSString,
+  textFromJSString,
+) where
 
 import Control.Monad.ST (stToIO)
+import Data.Array.Byte (
+  ByteArray (..),
+  MutableByteArray (..),
+ )
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
-import Data.Array.Byte
-  ( ByteArray(..)
-  , MutableByteArray(..)
-  )
 import Data.Text (Text)
-import qualified Data.Text.Array as A
-import qualified Data.Text.Internal as T
-import Foreign.Ptr
+import Data.Text.Array qualified as A
+import Data.Text.Internal qualified as T
 import Foreign.C.Types
-import GHC.Exts
-  ( ByteArray#
-  , MutableByteArray#
-  , RealWorld
-  )
+import Foreign.Ptr
+import GHC.Exts (
+  ByteArray#,
+  MutableByteArray#,
+  RealWorld,
+ )
 import GHC.Wasm.Prim
-import System.IO.Unsafe
-  ( unsafeDupablePerformIO
-  , unsafePerformIO
-  )
-
+import System.IO.Unsafe (
+  unsafeDupablePerformIO,
+  unsafePerformIO,
+ )
 
 -- Keep one encoder/decoder around rather than constructing one
 -- for every conversion.
 
-foreign import javascript unsafe
-  "new TextDecoder('utf-8', {fatal: true})"
+foreign import javascript unsafe "new TextDecoder('utf-8', {fatal: true})"
   js_newTextDecoder :: IO JSVal
 
-foreign import javascript unsafe
-  "new TextEncoder()"
+foreign import javascript unsafe "new TextEncoder()"
   js_newTextEncoder :: IO JSVal
 
 {-# NOINLINE textDecoder #-}
@@ -54,19 +50,18 @@ textDecoder = unsafePerformIO js_newTextDecoder
 textEncoder :: JSVal
 textEncoder = unsafePerformIO js_newTextEncoder
 
-
 -- Text -> JSString
 --
+
 -- $2 is the address of the ByteArray# payload in linear memory.
 
-foreign import javascript unsafe
-  "$1.decode(new Uint8Array(__exports.memory.buffer, $2 + $3, $4))"
-  js_decodeUtf8
-    :: JSVal
-    -> ByteArray#
-    -> Int             -- offset
-    -> Int             -- length
-    -> IO JSString
+foreign import javascript unsafe "$1.decode(new Uint8Array(__exports.memory.buffer, $2 + $3, $4))"
+  js_decodeUtf8 ::
+    JSVal ->
+    ByteArray# ->
+    Int -> -- offset
+    Int -> -- length
+    IO JSString
 
 textToJSStringIO :: Text -> IO JSString
 textToJSStringIO (T.Text (ByteArray ba#) off len) =
@@ -76,13 +71,12 @@ textToJSString :: Text -> JSString
 textToJSString =
   unsafeDupablePerformIO . textToJSStringIO
 
-foreign import javascript unsafe
-  "$1.decode(new Uint8Array(__exports.memory.buffer, $2, $3))"
-   js_decodeUtf8Ptr
-     :: JSVal
-     -> Ptr CChar
-     -> Int
-     -> IO JSString
+foreign import javascript unsafe "$1.decode(new Uint8Array(__exports.memory.buffer, $2, $3))"
+  js_decodeUtf8Ptr ::
+    JSVal ->
+    Ptr CChar ->
+    Int ->
+    IO JSString
 
 byteStringToJSString :: ByteString -> JSString
 byteStringToJSString bs = unsafeDupablePerformIO $
@@ -91,19 +85,17 @@ byteStringToJSString bs = unsafeDupablePerformIO $
 
 -- JSString -> Text
 
-foreign import javascript unsafe
-  "$1.length"
+foreign import javascript unsafe "$1.length"
   js_stringLength :: JSString -> IO Int
 
 -- $3 is the address of the MutableByteArray# payload.
-foreign import javascript unsafe
-  "$1.encodeInto($2, new Uint8Array(__exports.memory.buffer, $3, $4)).written"
-  js_encodeUtf8
-    :: JSVal
-    -> JSString
-    -> MutableByteArray# RealWorld
-    -> Int             -- destination capacity
-    -> IO Int          -- bytes written
+foreign import javascript unsafe "$1.encodeInto($2, new Uint8Array(__exports.memory.buffer, $3, $4)).written"
+  js_encodeUtf8 ::
+    JSVal ->
+    JSString ->
+    MutableByteArray# RealWorld ->
+    Int -> -- destination capacity
+    IO Int -- bytes written
 
 textFromJSStringIO :: JSString -> IO Text
 textFromJSStringIO s = do
